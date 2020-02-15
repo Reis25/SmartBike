3aaea152393b280b60b91f4ecd370f40ca06016c
Larissa Artemis <larissartemis@gmail.com>
2020-02-15 12:03:27 -0300

    tratamento de erros + função led_blink

--- app/src/output.c
+++ app/src/output.c
@@ -3,39 +3,74 @@
 
 #include "setup.h"
 
+/*
+ * 50 is flicker fusion threshold. Modulated light will be perceived
+ * as steady by our eyes when blinking rate is at least 50.
+ */
+#define PERIOD (USEC_PER_SEC / 50U)
+
+/* in micro second */
+#define FADESTEP	2000
+
+/*
+ * funções retornarão:
+ *  -> 0 se não houver erro
+ *  -> valor negativo se houver
+ */
+
 int output_open(output_t *out, char *dev_label) {
     out->dev_label = dev_label;
     out->device = device_get_binding(out->dev_label);
-    //printk("output_open done\n");
-     if(!out->device){
-        printk("Função: output_open (arquivo: ouput.c).\nFalha na atribução do out->device.\n");
-        return 1;
+
+     if(out->device == NULL){
+        printk("output_open: sem erros\n");
+        return 0;
+    } else {
+        printk("output_open: ERRO na atribução do out->device\n");
+        return -1;
     }
-    return 0;
 }
 
 int output_configure(output_t *out, u32_t pin, int flags) {
     out->pin = pin;
-    gpio_pin_configure(out->device, out->pin, flags);
-    //printk("output_configure done\n");
-    return 0;
+    int ret = gpio_pin_configure(out->device, out->pin, flags);
+    
+    if (ret == 0) {
+        printk("output_configure: sem erros\n");
+        return 0;
+    } else {
+        printk("output_configure: ERRO %d\n", ret);
+        return -1;
+    }
 }
 
 int output_set(output_t *out, u8_t value) {
     out->state = value;
-    gpio_pin_write(out->device, out->pin, out->state);
-    //printk("output_set done\n");
-    return 0;
+    int ret = gpio_pin_write(out->device, out->pin, out->state); // pros leds, se der problema, usar gpio_pin_set(out->device, out->pin, out->state)
+
+    if (ret == 0) {
+        printk("output_set: sem erros\n");
+        return 0;
+    } else {
+        printk("output_set: ERRO\n");
+        return -1;
+    }    
 }
 
 int output_pressed(output_t *out) {
-    if(out->state == 1) {
-        out->state = 0;
-        gpio_pin_write(out->device, out->pin, out->state);
+    out->state = (out->state) ? 0 : 1;
+    int ret = gpio_pin_write(out->device, out->pin, out->state);
+
+    if (ret == 0) {
+        printk("output_pressed: sem erros\n");
+        return 0;
     } else {
-        out->state = 1;
-        gpio_pin_write(out->device, out->pin, out->state);
-    }
-    //printk("output_pressed done\n");
-    return 0;
+        printk("output_pressed: ERRO\n");
+        return -1;
+    } 
+}
+
+int led_blink(output_t *out) {
+    u8_t value = (out->state) ? false : true; 
+    return output_set(out, value);
 }
